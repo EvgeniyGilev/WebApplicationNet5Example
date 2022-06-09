39112b46e3b53114113c38dd914a00f2856c6ec6
Евгений Гилев <euggil@yandex.ru>
2022-06-10 01:35:56 +0500

    пересобираем проект из теории

--- HomeApi.Contracts/Models/Devices/Validators/AddDeviceRequestValidator.cs
+++ HomeApi.Contracts/Models/Devices/Validators/AddDeviceRequestValidator.cs
@@ -7,27 +7,45 @@ using System.Threading.Tasks;
 
 namespace HomeApi.Contracts.Models.Devices.Validators
 {
+    /// <summary>
+    /// Класс-валидатор запросов подключения
+    /// </summary>
     public class AddDeviceRequestValidator : AbstractValidator<AddDeviceRequest>
     {
+        string[] _validLocations;
+
+        /// <summary>
+        /// Метод, конструктор, устанавливающий правила
+        /// </summary>
         public AddDeviceRequestValidator()
         {
-            RuleFor(x => x.CurrentVolts).
-                NotEmpty()
-                .Must(BeValidCurrentVolts)
-                .WithMessage("Напряжение у устройства должно быть от 120 до 220 В!");
+            // Сохраним список допустимых вариантов размещения устройств
+            _validLocations = new[]
+            {
+           "Kitchen",
+           "Bathroom",
+           "Livingroom",
+           "Toilet"
+       };
 
+            /* Зададим правила валидации */
+
+            RuleFor(x => x.Name).NotEmpty(); // Проверим на null и на пустое свойство
+            RuleFor(x => x.Manufacturer).NotEmpty();
+            RuleFor(x => x.Model).NotEmpty();
+            RuleFor(x => x.SerialNumber).NotEmpty();
+            RuleFor(x => x.CurrentVolts).NotEmpty().InclusiveBetween(120, 220); // Проверим, что значение в заданном диапазоне
+            RuleFor(x => x.GasUsage).NotNull();
+            RuleFor(x => x.Location).NotEmpty().Must(BeSupported).WithMessage($"Please choose one of the following locations: {string.Join(", ", _validLocations)}");
         }
 
-        // [Range(120, 220, ErrorMessage = "Поддерживаются устройства с напряжением от {1} до {2} вольт")]
-        public bool BeValidCurrentVolts(int volts)
+        /// <summary>
+        ///  Метод кастомной валидации для свойства location
+        /// </summary>
+        private bool BeSupported(string location)
         {
-            if (volts < 120)
-                return false;
-            if (volts > 220)
-                return false;
-
-            return true;
-
+            // Проверим, содержится ли значение в списке допустимых
+            return _validLocations.Any(e => e == location);
         }
     }
 }
