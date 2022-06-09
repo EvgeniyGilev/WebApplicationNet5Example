b8dbef573a7ec95b690ca0e3b2425a6651fdf264
Евгений Гилев <euggil@yandex.ru>
2022-06-10 03:10:12 +0500

    Реализовано удаление через метод DELETE

---
+++ HomeApi.Contracts/Models/Devices/DeleteDeviceRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeApi.Contracts.Models.Devices
+{
+    /// <summary>
+    /// Удаляем устройство
+    /// </summary>
+    public class DeleteDeviceRequest
+    {
+    }
+}
--- HomeApi/Controllers/DevicesController.cs
+++ HomeApi/Controllers/DevicesController.cs
@@ -129,5 +129,25 @@ namespace HomeApi.Controllers
 
             return StatusCode(200, $"Устройство изменено! Имя - {device.Name}, Серийный номер - {device.SerialNumber},  Комната подключения - {device.Room.Name}");
         }
+
+        /// <summary>
+        /// Удаление существующего устройства
+        /// </summary>
+        [HttpDelete]
+        [Route("{id}")]
+        public async Task<IActionResult> DeleteDevice(
+            [FromRoute] Guid id)
+        {
+            var device = await _devices.GetDeviceById(id);
+            if (device == null)
+                return StatusCode(400, $"Ошибка: Устройство с идентификатором {id} не существует.");
+
+
+            await _devices.DeleteDevice(
+                device
+            );
+
+            return StatusCode(200, $"Устройство удалено! Имя - {device.Name}");
+        }
     }
 }
